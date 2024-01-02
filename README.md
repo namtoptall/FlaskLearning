e879c7db821bcd4b0b66e62ddd698b526df875ec
Namtoptall <s3877256@rmit.edu.vn>
2024-01-02 09:24:11 +0700

    nnao?flaskntn?kholama?

--- README.md
+++ README.md
@@ -1,32 +1,9 @@
 ### this is a repo I create to learn flask and try tro implement flask into our project
-- POSIX Systems:
+- How to run : 
   ```bash
-  cd frontend
-  python -m venv .venv
-  source .venv/bin/activate
-  pip install -r ./requirements.txt
-  deactivate
-  ```
-- Windows Systems:
-  ```bash
-  cd frontend
   python -m venv venv
   venv/Script/activate
   pip install -r ./requirements.txt
+  python app.py
   deactivate
-  ```
-  
-From the project root directory:
-
-- POSIX Systems:
-  ```bash
-  cd frontend
-  source .venv/bin/activate
-  streamlit run ./main.py
-  ```
-- Windows Systems:
-  ```bash
-  cd frontend
-  venv/Script/activate
-  streamlit run ./main.py
   ```
--- app.py
+++ app.py
@@ -11,16 +11,16 @@ from dotenv import dotenv_values, load_dotenv
 
 load_dotenv()
 
-config = dotenv_values(".env")
+# config = dotenv_values(".env")
 # BACKEND_URI = config["BACKEND"]
-MONGODB_PASSWORD = os.getenv("MONGODB_PASSWORD")
+# MONGODB_PASSWORD = os.getenv("MONGODB_PASSWORD")
 
 app = Flask(__name__)
 
 # Connect to MongoDB
-client = pymongo.MongoClient(f"mongodb+srv://admin:{MONGODB_PASSWORD}@cluster0.zuy7zeb.mongodb.net/")
-db = client["testdb"]
-collection = db["test"]
+# client = pymongo.MongoClient(f"mongodb+srv://admin:{MONGODB_PASSWORD}@cluster0.zuy7zeb.mongodb.net/")
+# db = client["testdb"]
+# collection = db["test"]
 
 featured_products = [
     {"id": 1, "name": "Rose Bouquet", "description": "Beautiful arrangement of red roses.", "price": 29.99, "image": "imgs/000001.jpg"},
@@ -34,38 +34,38 @@ featured_products = [
     {"id": 9, "name": "Peace Lily", "description": "A peace lily plant.", "price": 19.99, "image": "imgs/000009.jpg"},
     {"id": 10, "name": "Hydrangea Bouquet", "description": "A bouquet of hydrangeas.", "price": 29.99, "image": "imgs/0000010.jpg"},
     {"id": 11, "name": "Sunflower Bouquet", "description": "A bouquet of sunflowers.", "price": 29.99, "image": "imgs/0000016.jpg"},
-    {"id": 12, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 34.99, "image": "imgs/0000017.jpg"},
-    {"id": 13, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 34.99, "image": "imgs/0000018.jpg"},
-    {"id": 14, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 34.99, "image": "imgs/0000019.jpg"},
-    {"id": 15, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 34.99, "image": "imgs/0000020.jpg"},
-    {"id": 16, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 34.99, "image": "imgs/0000021.jpg"},
-    {"id": 17, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 34.99, "image": "imgs/0000022.jpg"},
-    {"id": 18, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 34.99, "image": "imgs/0000023.jpg"},
-    {"id": 19, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 34.99, "image": "imgs/0000024.jpg"},
-    {"id": 20, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 34.99, "image": "imgs/0000025.jpg"},
+    {"id": 12, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 34.99, "image": "imgs/0000035.jpg"},
+    {"id": 13, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 35.99, "image": "imgs/0000036.jpg"},
+    {"id": 14, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 36.99, "image": "imgs/0000037.jpg"},
+    {"id": 15, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 37.99, "image": "imgs/0000040.jpg"},
+    {"id": 16, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 38.99, "image": "imgs/0000016.jpg"},
+    {"id": 17, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 39.99, "image": "imgs/0000017.jpg"},
+    {"id": 18, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 30.99, "image": "imgs/0000018.jpg"},
+    {"id": 19, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 3445.99, "image": "imgs/0000019.jpg"},
+    {"id": 20, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 34.99, "image": "imgs/0000020.jpg"},
 ]
 
 # Sample data for a product detail
-product_detail1={"id": 1, "name": "Rose Bouquet", "description": "Beautiful arrangement of red roses.", "price": 29.99, "image": "imgs/000001.jpg"}
-product_detail2={"id": 2, "name": "Lily Vase", "description": "Elegant vase with a mix of lilies.", "price": 39.99, "image": "imgs/000002.jpg"}
-product_detail3 = {"id": 3, "name": "Tulip Bouquet", "description": "Cheerful bouquet of tulips.", "price": 19.99, "image": "imgs/000003.jpg"},
-product_detail4 = {"id": 4, "name": "Daisy Vase", "description": "Bright and colorful daisies.", "price": 29.99, "image": "imgs/000004.jpg"},
-product_detail5 = {"id": 5, "name": "Carnation Bouquet", "description": "Traditional bouquet of carnations.", "price": 24.99, "image": "imgs/000005.jpg"},
-product_detail6 = {"id": 6, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 34.99, "image": "imgs/000006.jpg"},
-product_detail7 = {"id": 7, "name": "Orchid Plant", "description": "A beautiful orchid plant.", "price": 29.99, "image": "imgs/000007.jpg"},
-product_detail8 = {"id": 8, "name": "Succulent Plant", "description": "A succulent plant.", "price": 19.99, "image": "imgs/000008.jpg"},
-product_detail9 = {"id": 9, "name": "Peace Lily", "description": "A peace lily plant.", "price": 19.99, "image": "imgs/000009.jpg"},
-product_detail10 = {"id": 10, "name": "Hydrangea Bouquet", "description": "A bouquet of hydrangeas.", "price": 29.99, "image": "imgs/0000010.jpg"},
-product_detail11 = {"id": 11, "name": "Sunflower Bouquet", "description": "A bouquet of sunflowers.", "price": 29.99, "image": "imgs/0000016.jpg"},
-product_detail12 = {"id": 12, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 34.99, "image": "imgs/0000017.jpg"},
-product_detail13 = {"id": 13, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 34.99, "image": "imgs/0000017.jpg"},
-product_detail14 = {"id": 14, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 34.99, "image": "imgs/0000017.jpg"},
-product_detail15 = {"id": 15, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 34.99, "image": "imgs/0000017.jpg"},
-product_detail16 = {"id": 16, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 34.99, "image": "imgs/0000017.jpg"},
-product_detail17 = {"id": 17, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 34.99, "image": "imgs/0000017.jpg"},
-product_detail18 = {"id": 18, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 34.99, "image": "imgs/0000017.jpg"},
-product_detail19 = {"id": 19, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 34.99, "image": "imgs/0000017.jpg"},
-product_detail20 = {"id": 20, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 34.99, "image": "imgs/0000017.jpg"},
+product_detail1 = {"id": 1, "name": "Rose Bouquet", "description": "Beautiful arrangement of red roses.", "price": 29.99, "image": "imgs/000001.jpg"}
+product_detail2 = {"id": 2, "name": "Lily Vase", "description": "Elegant vase with a mix of lilies.", "price": 39.99, "image": "imgs/000002.jpg"}
+product_detail3 = {"id": 3, "name": "Tulip Bouquet", "description": "Cheerful bouquet of tulips.", "price": 19.99, "image": "imgs/000003.jpg"}
+product_detail4 = {"id": 4, "name": "Daisy Vase", "description": "Bright and colorful daisies.", "price": 29.99, "image": "imgs/000004.jpg"}
+product_detail5 = {"id": 5, "name": "Carnation Bouquet", "description": "Traditional bouquet of carnations.", "price": 24.99, "image": "imgs/000005.jpg"}
+product_detail6 = {"id": 6, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 34.99, "image": "imgs/000006.jpg"}
+product_detail7 = {"id": 7, "name": "Orchid Plant", "description": "A beautiful orchid plant.", "price": 29.99, "image": "imgs/000007.jpg"}
+product_detail8 = {"id": 8, "name": "Succulent Plant", "description": "A succulent plant.", "price": 19.99, "image": "imgs/000008.jpg"}
+product_detail9 = {"id": 9, "name": "Peace Lily", "description": "A peace lily plant.", "price": 19.99, "image": "imgs/000009.jpg"}
+product_detail10 = {"id": 10, "name": "Hydrangea Bouquet", "description": "A bouquet of hydrangeas.", "price": 29.99, "image": "imgs/0000010.jpg"}
+product_detail11 = {"id": 11, "name": "Sunflower Bouquet", "description": "A bouquet of sunflowers.", "price": 29.99, "image": "imgs/0000016.jpg"}
+product_detail12 = {"id": 12, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 34.99, "image": "imgs/0000035.jpg"}
+product_detail13 = {"id": 13, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 34.99, "image": "imgs/0000036.jpg"}
+product_detail14 = {"id": 14, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 34.99, "image": "imgs/0000037.jpg"}
+product_detail15 = {"id": 15, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 34.99, "image": "imgs/0000040.jpg"}
+product_detail16 = {"id": 16, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 34.99, "image": "imgs/0000016.jpg"}
+product_detail17 = {"id": 17, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 34.99, "image": "imgs/0000017.jpg"}
+product_detail18 = {"id": 18, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 34.99, "image": "imgs/0000018.jpg"}
+product_detail19 = {"id": 19, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 34.99, "image": "imgs/0000019.jpg"}
+product_detail20 = {"id": 20, "name": "Mixed Bouquet", "description": "Mixed bouquet of roses, lilies, and daisies.", "price": 34.99, "image": "imgs/0000020.jpg"}
 
 @app.route('/')
 def home():
@@ -73,16 +73,9 @@ def home():
 
 @app.route('/product/<int:product_id>')
 def product_detail(product_id):
-    product = {"name": "Rose Bouquet", "description": "Beautiful arrangement of red roses.", "price": 29.99, "image": "flower1.jpg"}
-    product_detail1={"id": 1, "name": "Rose Bouquet", "description": "Beautiful arrangement of red roses.", "price": 29.99, "image": "flower1.jpg"}
-    product_detail2={"id": 2, "name": "Lily Vase", "description": "Elegant vase with a mix of lilies.", "price": 39.99, "image": "flower2.jpg"}
-    product_detail3 = {"id": 3, "name": "Tulip Bouquet", "description": "Cheerful bouquet of tulips.", "price": 19.99, "image": "flower3.jpg"},
-    product_detail4 = {"id": 4, "name": "Daisy Vase", "description": "Bright and colorful daisies.", "price": 29.99, "image": "flower4.jpg"},
-    product_detail5 = {"id": 5, "name": "Carnation Bouquet", "description": "Traditional bouquet of carnations.", "price": 24.99, "image": "flower5.jpg"},
-    product = collection.find_one({"id": product_id})
-    
+    # product = collection.find_one({"id": product_id})
+    product = {} 
     return render_template('product_detail.html', product=product)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
